8c325531f68b043fb3061c34fefd7f4f663237d8
epage0328 <31248313+epage0328@users.noreply.github.com>
2019-10-15 20:55:39 -0500

    Add files via upload

---
+++ search bar/search bar/ViewController.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating
+{
+    
+    @IBOutlet var tableView: UITableView!
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredCakes = [Cake]()
+    
+    struct Cake {
+        var name = String()
+        var size = String()
+    }
+    
+    var cakes = [Cake(name: "Red Velvet", size: "Small"),
+                 Cake(name: "Brownie", size: "Medium"),
+                 Cake(name: "Bannna Bread", size: "Large"),
+                 Cake(name: "Vanilla", size: "Small"),
+                 Cake(name: "Minty", size: "Medium")]
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        filteredCakes = cakes
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.filteredCakes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = self.filteredCakes[indexPath.row].name
+        cell.detailTextLabel?.text = self.filteredCakes[indexPath.row].size
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        print("Row \(indexPath.row) selected")
+    }
+    
+    func updateSearchResults(for searchController: UISearchController)
+    {
+        // If we haven't typed anything into the search bar then do not filter the results
+        if searchController.searchBar.text! == ""
+        {
+            filteredCakes = cakes
+        }
+        else
+        {
+            // Filter the results
+            filteredCakes = cakes.filter
+                {
+                $0.name.lowercased().contains(searchController.searchBar.text!.lowercased())
+            }
+        }
+        self.tableView.reloadData()
+    }
+}
